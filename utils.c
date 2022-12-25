4c2df6d2cbed5ce6c349bc6a73d3f3e08a7e4c67
Your Name <you@example.com>
2022-12-25 16:09:39 -0500

    final

--- utils.c
+++ utils.c
@@ -1,62 +1,65 @@
 #include "utils.h"
 
+#define SIZE_T 8
 
-size_t MAX(size_t x, size_t y)
+
+inline size_t MAX(size_t x, size_t y)
 {
     return ((x) > (y)? (x) : (y));
 }
 
 /* Pack a size and allocated bit into a word */
-size_t PACK(size_t size, size_t alloc)
+inline size_t PACK(size_t size, size_t alloc)
 {
     return  ((size) | (alloc));//warning: unused variable ‘boundary_tag’
 }                                                             // line:vm:mm:pack
 
 /* Read and write a word at address p */
-size_t GET(void *p)                                           // line:vm:mm:get
+inline size_t GET(void *p)                                           // line:vm:mm:get
 {
     return  (*(unsigned int *)(p));
 }
 
-void PUT(void *p, size_t val)                                 // line:vm:mm:put
+inline void PUT(void *p, size_t val)                                 // line:vm:mm:put
 {
     (*(unsigned int *)(p) = (val));
 }
 
 /* Read the size and allocated fields from address p */
-size_t GET_SIZE(void *p)
+inline size_t GET_SIZE(void *p)
 {
     return (GET(p) & ~0x7); // line:vm:mm:getsize
 }
 
-size_t GET_ALLOC(void *p)
+inline size_t GET_ALLOC(void *p)
 {
     return (GET(p) & 0x1); // line:vm:mm:getalloc
 }
 
 //The remaining macros operate on block pointers (denoted bp) that point to the first payload byte.
 /* Given block ptr bp, compute address of its header and footer */
-void* HDRP(void *bp)
+inline void* HDRP(void *bp)
 {
     return ((char *)(bp)-2*WSIZE);// line:vm:mm:hdrp
 }
 
-void* FTRP(void *bp)
+inline void* FTRP(void *bp)
 {
     return ((char *)(bp) + GET_SIZE(HDRP(bp)) - 2*DSIZE); // line:vm:mm:ftrp
 }
 
 /* Given block ptr bp, compute address of next and previous blocks */
-void* NEXT_BLKP(void *bp)
+inline void* NEXT_BLKP(void *bp)
 {
     return ((char *)(bp) + GET_SIZE(((char *)(bp)-2*WSIZE))); // line:vm:mm:nextblkp
 }
 
 
-void* PREV_BLKP(void *bp)
+inline void* PREV_BLKP(void *bp)
 {
     return ((char *)(bp)-GET_SIZE(((char *)(bp)-2*DSIZE))); // line:vm:mm:prevblkp
 }
+
 /* $end mallocmacros */
 
 /* Function: roundup
@@ -64,4 +67,39 @@ void* PREV_BLKP(void *bp)
  * This function rounds up the given number to the given multiple, which
  * must be a power of 2, and returns the result.
  */
-size_t roundup(size_t sz, size_t mult) { return (sz + (mult) + mult - 1) & ~(mult - 1); }
+ size_t roundup(size_t sz, size_t mult) { return (sz + (mult) + mult - 1) & ~(mult - 1); }
+
+
+/* Given block ptr bp, compute address of next and previous pointers */
+
+/*void* PREV_PTR(void *bp)
+{
+    return *(char **)((char*)bp+2*WSIZE); // line:vm:mm:nextblkp
+    //return (*(char **) (bp + (2*WSIZE) ) );
+}
+
+void* NEXT_PTR(void *bp)
+{
+    return (*(char **)(bp)); // line:vm:mm:prevblkp
+}*/
+
+inline char *getprevptr(char *ptr)
+{
+    return (char *)*(size_t*)(ptr);
+}
+inline void setprevptr(char *ptr, char *prev)
+{
+    //ptr += SIZE_T;
+    *(size_t*)ptr = (size_t)prev;
+}
+
+inline char *getnextptr(char *ptr)
+{
+    return (char *)*(size_t*)(ptr + SIZE_T);;
+}
+
+inline void setnextptr(char *ptr, char *next)
+{
+    ptr += SIZE_T;
+    *(size_t*)ptr = (size_t)next;
+}
